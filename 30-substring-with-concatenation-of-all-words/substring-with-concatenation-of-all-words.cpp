c815b1bc3d700d46e8de54fb5fe180249dbd8ff0
Harshita Choudhary <114794269+harshita1222@users.noreply.github.com>
2024-12-30 11:17:17 +0530

    Time: 26 ms (48.12%) | Memory: 24 MB (32.34%) - LeetSync

---
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.cpp
@@ -0,0 +1,60 @@
+class Solution {
+    // This is the reference map.
+    std::unordered_map<std::string, unsigned int> map;
+public:
+    vector<int> findSubstring(string s, vector<string>& words) {
+        std::vector<int> result;
+
+        // Length of each word in words.
+        unsigned int length = words[0].size();
+
+        // Initializing the reference map.
+        map.clear();
+        for (const std::string& word : words)
+            map[word]++;
+
+        // Iterate for each offset
+        // As many times as the number of characters in each word.
+        for (unsigned int offset = 0; offset < length; ++offset) {
+            // Sliding window size.
+            unsigned int size = 0;
+            // Sliding window memory.
+            std::unordered_map<std::string, unsigned int> seen;
+
+            // Iterate over the string, with the step equals to length.
+            for (unsigned int i = offset; i + length <= s.size(); i += length) {
+                // This is the word we're going to check.
+                std::string sub = s.substr(i, length);
+
+                // If the word is absent in the reference map,
+                // we clear the sliding window and move on.
+                auto itr = map.find(sub);
+                if (itr == map.end()) {
+                    seen.clear();
+                    size = 0;
+                    continue;
+                }
+
+               
+                ++seen[sub];
+                ++size;
+
+               
+                while (seen[sub] > itr->second) {
+                  
+                    std::string first = s.substr(i - (size - 1) * length, length);
+
+                  
+                    --seen[first];
+                    --size;
+                }
+                
+               
+                if (size == words.size())
+                    result.push_back(i - (size - 1) * length);
+            }
+        }
+
+        return result;
+    }
+};
