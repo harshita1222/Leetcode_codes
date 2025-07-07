1eea910b1249dd9fcb8e53decb2c6e82eb1f22d5
Harshita Choudhary <114794269+harshita1222@users.noreply.github.com>
2025-07-07 16:48:51 +0530

    Time: 7 ms (99.39%) | Memory: 56.5 MB (95.14%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,22 +1,22 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
         int n = heights.length;
-        if(n == 0) return 0; // Base Condition
+        if(n == 0) return 0; 
         int maxArea = 0;
-        int left[] = new int[n]; //fill left boundary
-        int right[] = new int[n]; // fill right boundary
+        int left[] = new int[n]; 
+        int right[] = new int[n]; 
         
         left[0] = -1;
         right[n - 1] = n;
         
         for(int i = 1; i < n; i++){
-            int prev = i - 1; // previous for comparing the heights
+            int prev = i - 1; 
             while(prev >= 0 && heights[prev] >= heights[i]){
-                prev = left[prev]; // we have done this to minimise the jumps we make to the left
+                prev = left[prev]; 
             }
             left[i] = prev;
         }
-        // Similarly we do for right
+     
         for(int i = n - 2; i >= 0; i--){
             int prev = i + 1; 
             while(prev < n && heights[prev] >= heights[i]){
@@ -24,7 +24,7 @@ class Solution {
             }
             right[i] = prev;
         }
-        // once we have these two arrays fill we need width & area
+       
         for(int i = 0; i < n; i++){
             int width = right[i] - left[i] - 1;
             maxArea = Math.max(maxArea, heights[i] * width);
